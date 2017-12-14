d6c1f239a55c940d81dc3f201ee938046f23755d
MemoryAndDream <312141830@qq.com>
2017-12-14 11:46:54 +0800

    up

--- devnav/wechat/service/reply.py
+++ devnav/wechat/service/reply.py
@@ -94,14 +94,15 @@ def search_resource(queryString,userOpenId='',mod=''):
         import re
         if re.match('\d+',queryString):
             start = now - datetime.timedelta(hours=23, minutes=59, seconds=59)
-            resources = Resource_Cache.objects.filter(create_time__gt=start).filter(OpenID__iexact=userOpenId).filter(title__endswith=queryString + '_' + mod).order_by("-create_time")[:10]
+            resources = Resource_Cache.objects.filter(create_time__gt=start).filter(title__endswith=' '+queryString + '_' + mod).order_by("-create_time")[:5]
 
         else:
-            start = now-datetime.timedelta(hours=23, minutes=59, seconds=59)#缓存一小时的数据 缓存没有继续搜索的功能。。。
+            start = now-datetime.timedelta(hours=23, minutes=59, seconds=59)#缓存一小时的数据 读取缓存需要修改用户id  缓存和上面的逻辑有冲突
             resources = Resource_Cache.objects.filter(create_time__gt=start).filter(keyword__iexact=queryString+'_'+mod)[:10]#后面需要加更多限制 反正也显示不了10条
         result=[]
         for resource in resources:
             result.append('''<a href='%s'>%s</a> '''%(resource.url,resource.title))
+            resource.update(create_time=now)
         output = results_toString(result)
     except Exception,e:
         logger.error(str(e))
