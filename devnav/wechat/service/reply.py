02bec5c3ade5e8f8313ffba7d9658fe35ef4fd66
MemoryAndDream <312141830@qq.com>
2017-12-14 13:46:21 +0800

    up

--- devnav/wechat/service/reply.py
+++ devnav/wechat/service/reply.py
@@ -96,15 +96,15 @@ def search_resource(queryString,userOpenId='',mod=''):
         import re
         if re.match('\d+',queryString):
             start = now - datetime.timedelta(hours=23, minutes=59, seconds=59)
-            resources = Resource_Cache.objects.filter(OpenID__iexact=userOpenId).filter(create_time__gt=start).filter(title__endswith=' '+queryString + '_' + mod).order_by("-create_time")[:10]
+            resources = Resource_Cache.objects.filter(OpenID__iexact=userOpenId).filter(create_time__gt=start).filter(title__endswith=' '+queryString + '_' + mod).order_by("-create_time").distinct('url')[:10]
 
         else:
             start = now-datetime.timedelta(hours=23, minutes=59, seconds=59)#缓存一小时的数据 读取缓存需要修改用户id  缓存和上面的逻辑有冲突
-            resources = Resource_Cache.objects.filter(create_time__gt=start).filter(keyword__iexact=queryString+'_'+mod)[:10]#后面需要加更多限制 反正也显示不了10条
+            resources = Resource_Cache.objects.filter(create_time__gt=start).filter(keyword__iexact=queryString+'_'+mod).distinct('url')[:10]#后面需要加更多限制 反正也显示不了10条
         result=[]
 
         for resource in resources:
-            result.append('''<a href='%s'>%s</a> '''%(resource.url,resource.title))
+            result.append('''<a href='%s'>%s</a> '''%(resource.url,resource.title.replace('_' + mod,'')))
             resource.create_time = now
             resource.OpenID = userOpenId
             resource.save()
